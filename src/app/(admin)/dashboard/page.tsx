5d722cd49dbd25a7360593d7cdde7824f9733771
VishalBala@Office-Jeevijay <vishal.bala.100@gmail.com>
2025-03-10 18:05:01 +0530

    DSB impo fix 4

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,7 +1,9 @@
 // import DashBoard from "@/components/admin/dashboard/Sales";
 // import DashBoard from "@/components/admin/dashboard/DashBoard";
 // import DashBoard from "@/components/admin/dashboard/Dashboard";
-import DashBoard from "../../../components/admin/dashboard/Dashboard";
+// import DashBoard from "../../../components/admin/dashboard/Dashboard";
+"use client";
+import DashBoard from "@/components/admin/dashboard/Dashboard";
 import React from "react";
 
 const Page = () => {
